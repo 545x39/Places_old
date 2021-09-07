c410d75ebca7b9b060a337a0332c2406fd0a1069
545x39 <rat.the.drummer@gmail.com>
2021-09-07 07:38:11 +0300

    Buildscript update.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,26 @@ import dependencies.Application.MIN_SDK
 import dependencies.Application.TARGET_SDK
 import dependencies.Application.VERSION_CODE
 import dependencies.Application.VERSION_NAME
+import dependencies.Versions.APPCOMPAT_VERSION
+import dependencies.Versions.CORE_VERSION
+import dependencies.Versions.COROUTINES_VERSION
+import dependencies.Versions.DAGGER_VERSION
+import dependencies.Versions.ESPRESSO_VERSION
+import dependencies.Versions.GSON_VERSION
+import dependencies.Versions.INTERCEPTOR_VERSION
+import dependencies.Versions.JUNIT_EXT_VERSION
+import dependencies.Versions.JUNIT_VERSION
+import dependencies.Versions.LIFECYCLE_EXT_VERSION
 import dependencies.Versions.LIFECYCLE_VERSION
+import dependencies.Versions.MATERIAL_VERSION
+import dependencies.Versions.NAVIGATION_VERSION
+import dependencies.Versions.PLAY_CORE_VERSION
+import dependencies.Versions.PREFERENCE_VERSION
+import dependencies.Versions.RETROFIT_VERSION
+import dependencies.Versions.ROOM_VERSION
+import dependencies.Versions.TIMBER_VERSION
+import dependencies.Versions.WORK_MANAGER_VERSION
+
 
 //<editor-fold defaultstate="collapsed" desc="PLUGINS">
 plugins {
@@ -14,10 +33,10 @@ plugins {
 }
 //</editor-fold>
 
-
+//<editor-fold defaultstate="collapsed" desc="ANDROID">
 android {
 
-//<editor-fold defaultstate="collapsed" desc="DEFAULT CONFIG">
+    //<editor-fold defaultstate="collapsed" desc="DEFAULT CONFIG">
     defaultConfig {
         applicationId = APPLICATION_ID
         minSdk = MIN_SDK
@@ -27,7 +46,9 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         compileSdk = COMPILE_SDK
     }
-//</editor-fold">
+    //</editor-fold">
+
+    //<editor-fold defaultstate="collapsed" desc="BUILD TYPES">
     buildTypes {
         release {
             isMinifyEnabled = true
@@ -37,78 +58,74 @@ android {
             )
         }
     }
+    //</editor-fold">
+
+    //<editor-fold defaultstate="collapsed" desc="COMPILE OPTIONS">
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    //</editor-fold">
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
     }
+
     setDynamicFeatures(setOf(":map", ":search", ":settings", ":objectdetails"))
 }
+//</editor-fold>
 
+//<editor-fold defaultstate="collapsed" desc="DEPENDENCIES">
 dependencies {
-    ////VERSIONS
-    val nav_version = "2.3.5"
-    val dagger_version = "2.38.1"
-    //noinspection SpellCheckingInspection
-    val coroutines_version = "1.5.2-native-mt"
-    val work_manager_version = "2.7.0-beta01"
-    val preference_version = "1.1.1"
-    val room_version = "2.3.0"
-    val retrofit_version = "2.9.0"
-    val gson_version = "2.8.6"
-    val play_core_version = "1.10.1"
-    val timber_version = "5.0.1"
     /**CORE */
-    implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
+    implementation("androidx.core:core-ktx:$CORE_VERSION")
+    implementation("androidx.appcompat:appcompat:$APPCOMPAT_VERSION")
+    implementation("com.google.android.material:material:$MATERIAL_VERSION")
     /** VIEW MODEL AND LIVE DATA */
     implementation("androidx.lifecycle:lifecycle-extensions:$LIFECYCLE_VERSION")
     /** DAGGER */
-    implementation("com.google.dagger:dagger:$dagger_version")
-    kapt("com.google.dagger:dagger-compiler:$dagger_version")
-    kapt("com.google.dagger:dagger-android-processor:$dagger_version")
+    implementation("com.google.dagger:dagger:$DAGGER_VERSION")
+    kapt("com.google.dagger:dagger-compiler:$DAGGER_VERSION")
+    kapt("com.google.dagger:dagger-android-processor:$DAGGER_VERSION")
     /** ROOM */
-    implementation("androidx.room:room-runtime:$room_version")
-    kapt("androidx.room:room-compiler:$room_version")
-    implementation("androidx.room:room-ktx:$room_version")
+    implementation("androidx.room:room-runtime:$ROOM_VERSION")
+    kapt("androidx.room:room-compiler:$ROOM_VERSION")
+    implementation("androidx.room:room-ktx:$ROOM_VERSION")
     //Both RxJava dependencies seem to be required for usage with androidx
-//    implementation("androidx.room:room-rxjava2:$room_version")
+//    implementation("androidx.room:room-rxjava2:$ROOM_VERSION")
     /** RETROFIT */
-    implementation("com.squareup.retrofit2:retrofit:$retrofit_version")
-    implementation("com.squareup.retrofit2:converter-gson:$retrofit_version")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
-    implementation("com.squareup.retrofit2:converter-scalars:$retrofit_version")
+    implementation("com.squareup.retrofit2:retrofit:$RETROFIT_VERSION")
+    implementation("com.squareup.retrofit2:converter-gson:$RETROFIT_VERSION")
+    implementation("com.squareup.okhttp3:logging-interceptor:$INTERCEPTOR_VERSION")
+    implementation("com.squareup.retrofit2:converter-scalars:$RETROFIT_VERSION")
     /** GSON */
-    implementation("com.google.code.gson:gson:$gson_version")
-    //
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+    implementation("com.google.code.gson:gson:$GSON_VERSION")
     /** Coroutine lifecycle extensions */
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$LIFECYCLE_EXT_VERSION")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_EXT_VERSION")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$LIFECYCLE_EXT_VERSION")
     /** TIMBER */
-    implementation("com.jakewharton.timber:timber:$timber_version")
+    implementation("com.jakewharton.timber:timber:$TIMBER_VERSION")
     /** COROUTINES */
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines_version")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$COROUTINES_VERSION")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$COROUTINES_VERSION")
     /** WORK MANAGER (+ coroutines) */
-    implementation("androidx.work:work-runtime-ktx:$work_manager_version")
+    implementation("androidx.work:work-runtime-ktx:$WORK_MANAGER_VERSION")
     /** PREFERENCES */
-    implementation("androidx.preference:preference-ktx:$preference_version")
+    implementation("androidx.preference:preference-ktx:$PREFERENCE_VERSION")
     /** NAVIGATION COMPONENT */
-    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
-    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
-    api("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-fragment-ktx:$NAVIGATION_VERSION")
+    implementation("androidx.navigation:navigation-ui-ktx:$NAVIGATION_VERSION")
+    api("androidx.navigation:navigation-dynamic-features-fragment:$NAVIGATION_VERSION")
     /** PLAY CORE */
-    api("com.google.android.play:core:$play_core_version")
+    api("com.google.android.play:core:$PLAY_CORE_VERSION")
     /** TESTING */
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    testImplementation("junit:junit:$JUNIT_VERSION")
+    androidTestImplementation("androidx.test.ext:junit:$JUNIT_EXT_VERSION")
+    androidTestImplementation("androidx.test.espresso:espresso-core:$ESPRESSO_VERSION")
 }
+//</editor-fold>
