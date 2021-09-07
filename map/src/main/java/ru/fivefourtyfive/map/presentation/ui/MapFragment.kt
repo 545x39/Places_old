141f2f1a6f9fbf8b6baf1e74d62f57ac9cfa6c70
dmitry <dseliverstov@stc-spb.ru>
2021-09-07 13:50:31 +0300

    Changed map source to Wikimedia's no-labeled one.

--- map/src/main/java/ru/fivefourtyfive/map/presentation/ui/MapFragment.kt
+++ map/src/main/java/ru/fivefourtyfive/map/presentation/ui/MapFragment.kt
@@ -13,7 +13,13 @@ import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
 import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
 import org.osmdroid.config.Configuration
+import org.osmdroid.events.DelayedMapListener
+import org.osmdroid.events.MapListener
+import org.osmdroid.events.ScrollEvent
+import org.osmdroid.events.ZoomEvent
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.tileprovider.tilesource.TileSourcePolicy
+import org.osmdroid.tileprovider.tilesource.XYTileSource
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.compass.CompassOverlay
@@ -25,6 +31,7 @@ import ru.fivefourtyfive.map.R
 import ru.fivefourtyfive.wikimapper.R as appR
 import ru.fivefourtyfive.wikimapper.BuildConfig
 import ru.fivefourtyfive.wikimapper.data.datasource.remote.util.Parameter.ID
+import timber.log.Timber
 import java.io.File
 import kotlin.random.Random
 
@@ -57,11 +64,23 @@ class MapFragment : Fragment() {
     override fun onResume() {
         super.onResume()
         setMap()
+        setMapListener()
     }
+
+    val NO_LABELS = XYTileSource(
+        "OsmNoLabels", 0, 19, 256, ".png",
+        arrayOf("https://c.tiles.wmflabs.org/osm-no-labels/"),
+        "© OpenStreetMap contributors",
+        TileSourcePolicy(
+            2,
+            TileSourcePolicy.FLAG_NO_BULK or TileSourcePolicy.FLAG_NO_PREVENTIVE or TileSourcePolicy.FLAG_USER_AGENT_MEANINGFUL or TileSourcePolicy.FLAG_USER_AGENT_NORMALIZED
+        )
+    )
 
     //<editor-fold defaultstate="collapsed" desc="MAP">
     private fun setMap() {
         val context: Context = mapView.context
+
         fun setCompassOverlay() {
             CompassOverlay(context, InternalCompassOrientationProvider(context), mapView).apply {
                 enableCompass()
@@ -75,7 +94,8 @@ class MapFragment : Fragment() {
             File(requireActivity().applicationContext.getExternalFilesDir("osmdroid")!!.absolutePath)
         configuration.osmdroidTileCache = File(configuration.osmdroidBasePath, "tiles")
         mapView.apply {
-            setTileSource(TileSourceFactory.MAPNIK)
+//            setTileSource(TileSourceFactory.MAPNIK)
+            setTileSource(NO_LABELS)
             setUseDataConnection(true)
             isTilesScaledToDpi = true
             setMultiTouchControls(true)
@@ -83,6 +103,8 @@ class MapFragment : Fragment() {
             maxZoomLevel = 20.0
             zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
             controller.setZoom(10.0)
+            isVerticalMapRepetitionEnabled = false
+            isHorizontalMapRepetitionEnabled = false
 //        zoomController.setZoomInEnabled(false)
 //        zoomController.setZoomOutEnabled(false)
         }
@@ -103,7 +125,22 @@ class MapFragment : Fragment() {
     }
 //</editor-fold>
 
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) = inflater.inflate(R.menu.menu_map, menu)
+    private fun setMapListener() {
+        mapView.addMapListener(DelayedMapListener(object : MapListener {
+            override fun onScroll(event: ScrollEvent?): Boolean {
+                Timber.e("MAP SCROLLED")
+                return false
+            }
+
+            override fun onZoom(event: ZoomEvent?): Boolean {
+                Timber.e("MAP RESIZED")
+                return false
+            }
+        }, 200))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) =
+        inflater.inflate(R.menu.menu_map, menu)
 
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -115,7 +152,7 @@ class MapFragment : Fragment() {
         return super.onOptionsItemSelected(item)
     }
 
-    private fun navigateToSearch(){
+    private fun navigateToSearch() {
         val installMonitor = DynamicInstallMonitor()
         findNavController().navigate(
             appR.id.action_mapFragment_to_searchFragment,
@@ -126,7 +163,7 @@ class MapFragment : Fragment() {
         observeInstall(installMonitor)
     }
 
-    private fun navigateToObjectDetails(){
+    private fun navigateToObjectDetails() {
         val installMonitor = DynamicInstallMonitor()
         findNavController().navigate(
             appR.id.action_mapFragment_to_placeDetailsFragment,
