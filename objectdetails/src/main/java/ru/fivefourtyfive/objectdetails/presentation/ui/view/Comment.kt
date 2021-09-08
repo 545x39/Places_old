7231dfe7f23d7c8f8c679268e66ab24a41214288
dmitry <dseliverstov@stc-spb.ru>
2021-09-08 20:06:55 +0300

    Place details fragments menu added, compose dependencies added.

---
+++ objectdetails/src/main/java/ru/fivefourtyfive/objectdetails/presentation/ui/view/Comment.kt
@@ -0,0 +1,16 @@
+package ru.fivefourtyfive.objectdetails.presentation.ui.view
+
+//import androidx.compose.runtime.Composable
+//import androidx.compose.ui.Modifier
+
+import ru.fivefourtyfive.wikimapper.domain.entity.Comment
+
+//class C {
+//    @Composable//comment: Comment
+//    fun Comment() {
+//        Column(Modifier.fillMaxSize()) {
+//            Text("Look at this CustomView!")
+//        }
+//
+//    }
+//}
