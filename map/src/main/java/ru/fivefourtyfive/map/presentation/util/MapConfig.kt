32ca5fe46f51d02b5dc009e11f2d4ff7c5c08dd9
dmitry <dseliverstov@stc-spb.ru>
2021-09-15 17:20:46 +0300

    Cleanup.

--- map/src/main/java/ru/fivefourtyfive/map/presentation/util/MapConfig.kt
+++ map/src/main/java/ru/fivefourtyfive/map/presentation/util/MapConfig.kt
@@ -1,66 +1,33 @@
 package ru.fivefourtyfive.map.presentation.util
 
-import android.content.Context
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourcePolicy
 import org.osmdroid.tileprovider.tilesource.XYTileSource
 import org.osmdroid.views.CustomZoomButtonsController
 import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.compass.CompassOverlay
-import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider
-import org.osmdroid.views.overlay.mylocation.DirectedLocationOverlay
-import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
-import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import ru.fivefourtyfive.wikimapper.BuildConfig
 import ru.fivefourtyfive.wikimapper.util.Network
 import java.io.File
 
 object MapConfig {
 
-    fun config(context: Context, mapView: MapView){
-
-        fun setCompassOverlay() {
-            CompassOverlay(context, InternalCompassOrientationProvider(context), mapView).apply {
-                enableCompass()
-                mapView.overlays.add(this)
-            }
+    fun MapView.config() {
+        Configuration.getInstance().apply {
+            userAgentValue = BuildConfig.APPLICATION_ID
+            osmdroidBasePath = File(context.getExternalFilesDir("osmdroid")!!.absolutePath)
+            osmdroidTileCache = File(osmdroidBasePath, "tiles")
         }
-        val locationOverlay: MyLocationNewOverlay?
-        val locationProvider: GpsMyLocationProvider?
-        val configuration = Configuration.getInstance()
-        configuration.userAgentValue = BuildConfig.APPLICATION_ID
-        configuration.osmdroidBasePath =
-            File(context.applicationContext.getExternalFilesDir("osmdroid")!!.absolutePath)
-        configuration.osmdroidTileCache = File(configuration.osmdroidBasePath, "tiles")
-        mapView.apply {
-//            setTileSource(TileSourceFactory.MAPNIK)
-            setTileSource(wikimediaTileSource)
-            setUseDataConnection(true)
-            isTilesScaledToDpi = true
-            setMultiTouchControls(true)
-            minZoomLevel = 2.0
-            maxZoomLevel = 19.0
-            zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
-            controller.setZoom(10.0)
-            isVerticalMapRepetitionEnabled = false
-            isHorizontalMapRepetitionEnabled = false
-//        zoomController.setZoomInEnabled(false)
-//        zoomController.setZoomOutEnabled(false)
-        }
-        setCompassOverlay()
-        ///
-        locationProvider = GpsMyLocationProvider(context)
-        locationOverlay = MyLocationNewOverlay(locationProvider, mapView)
-        val directedLocationOverlay = DirectedLocationOverlay(context)
-        ////
-        mapView.overlays.add(locationOverlay)
-        mapView.overlays.add(directedLocationOverlay)
-        locationOverlay.apply {
-            enableMyLocation()
-            enableFollowLocation()
-//        enableAutoStop = false
-        }
-        mapView.invalidate()
+        setTileSource(wikimediaTileSource)
+        setUseDataConnection(true)
+        setMultiTouchControls(true)
+        isTilesScaledToDpi = true
+        minZoomLevel = 2.0
+        maxZoomLevel = 19.0
+        zoomController.setVisibility(CustomZoomButtonsController.Visibility.SHOW_AND_FADEOUT)
+        controller.setZoom(10.0)
+        isVerticalMapRepetitionEnabled = false
+        isHorizontalMapRepetitionEnabled = false
+        invalidate()
     }
 
     private val wikimediaTileSource = XYTileSource(
