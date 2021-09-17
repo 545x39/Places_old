d70e118c7ac27b74d09053ee4af1afd54221684a
545x39 <rat.the.drummer@gmail.com>
2021-09-17 07:00:09 +0300

    API key added.

--- app/src/main/java/ru/fivefourtyfive/wikimapper/util/Const.kt
+++ app/src/main/java/ru/fivefourtyfive/wikimapper/util/Const.kt
@@ -5,6 +5,7 @@ import android.Manifest.permission.*
 object Network {
     const val ROOT_URL = "https://wikimapia.org"
     const val API_URL = "http://api.wikimapia.org/"
+    const val API_KEY = "F29DE311-79E27525-6803266D-365D72DD-2A145EEB-9C8F7110-26ACA826-139C9859"
     const val WIKIMEDIA_TILES_URL = "https://c.tiles.wmflabs.org/osm-no-labels/"
     const val CONNECT_TIMEOUT = 120L
     const val READ_TIMEOUT = 120L
