522ec6dc2cf2649fcd981878c5968a4e589c772d
545x39 <rat.the.drummer@gmail.com>
2021-09-20 21:32:47 +0300

    Upd.

--- app/src/main/java/ru/fivefourtyfive/wikimapper/domain/entity/PolyPoint.kt
+++ app/src/main/java/ru/fivefourtyfive/wikimapper/domain/entity/PolyPoint.kt
@@ -3,7 +3,7 @@ package ru.fivefourtyfive.wikimapper.domain.entity
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-data class Dot(
+data class PolyPoint(
     @SerializedName("x")
     @Expose
     val x: Double,
