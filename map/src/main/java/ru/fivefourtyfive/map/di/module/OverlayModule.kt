0ff92f95f99951a82260a92939fc96b6e9e3722b
dmitry <dseliverstov@stc-spb.ru>
2021-09-30 19:26:38 +0300

    My location icon and color changed.

--- map/src/main/java/ru/fivefourtyfive/map/di/module/OverlayModule.kt
+++ map/src/main/java/ru/fivefourtyfive/map/di/module/OverlayModule.kt
@@ -1,7 +1,11 @@
 package ru.fivefourtyfive.map.di.module
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.graphics.Color
+import android.graphics.drawable.VectorDrawable
+import androidx.core.content.res.ResourcesCompat
 import dagger.Module
 import dagger.Provides
 import org.osmdroid.tileprovider.MapTileProviderBasic
@@ -16,12 +20,11 @@ import org.osmdroid.views.overlay.gestures.RotationGestureOverlay
 import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import ru.fivefourtyfive.map.R
 import ru.fivefourtyfive.map.di.MapFragmentScope
-import ru.fivefourtyfive.map.presentation.util.Overlay
 import ru.fivefourtyfive.map.presentation.util.Overlay.IMAGERY_LABELS_OVERLAY
 import ru.fivefourtyfive.map.presentation.util.Overlay.TRANSPORTATION_OVERLAY
 import ru.fivefourtyfive.map.presentation.util.Overlay.WIKIMAPIA_OVERLAY
-import ru.fivefourtyfive.map.presentation.util.TileSource
 import ru.fivefourtyfive.map.presentation.util.TileSource.ARCGIS_IMAGERY_LABELS_TILE_SOURCE
 import ru.fivefourtyfive.map.presentation.util.TileSource.ARCGIS_IMAGERY_TRANSPORTATION_TILE_SOURCE
 import ru.fivefourtyfive.map.presentation.util.TileSource.WIKIMAPIA_TILE_SOURCE
@@ -36,10 +39,38 @@ class OverlayModule {
 
     @MapFragmentScope
     @Provides
-    fun provideMyLocationOverlay(provider: GpsMyLocationProvider, mapView: MapView) =
-        MyLocationNewOverlay(provider, mapView).apply {
+    fun provideMyLocationOverlay(
+        provider: GpsMyLocationProvider,
+        mapView: MapView
+    ): MyLocationNewOverlay {
+
+        fun VectorDrawable.toBitmap(): Bitmap? {
+            val bitmap = Bitmap.createBitmap(
+                intrinsicWidth,
+                intrinsicHeight, Bitmap.Config.ARGB_8888
+            )
+            Canvas(bitmap).apply {
+                setBounds(0, 0, width, height)
+                draw(this)
+            }
+            return bitmap
+        }
+
+        fun getBitmap(resId: Int) = (ResourcesCompat.getDrawable(
+            mapView.context.resources,
+            resId,
+            null
+        ) as VectorDrawable?)!!.toBitmap()
+
+        return MyLocationNewOverlay(provider, mapView).apply {
+//            setPersonIcon(getBitmap(R.drawable.icon_my_location))
+            setDirectionArrow(getBitmap(R.drawable.icon_my_location), getBitmap(R.drawable.icon_arrow))
+            val scale = mapView.context.resources.displayMetrics.density
+            //mPersonHotspot = new PointF(24.0f * mScale + 0.5f, 39.0f * mScale + 0.5f);
+            setPersonHotspot(12.0f * scale - 0.5f, 12.0f * scale - 0.5f)
             enableMyLocation()
         }
+    }
 
     @MapFragmentScope
     @Provides
