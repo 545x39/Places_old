5a1ffd6e92472b5b113aefa5e6e742e980d04c57
d <rat.the.drummer@gmail.com>
2022-03-02 19:27:22 +0300

    Dependencies versions updated.

--- objectdetails/src/main/java/ru/fivefourtyfive/objectdetails/domain/usecase/implementation/GetPlaceUseCase.kt
+++ objectdetails/src/main/java/ru/fivefourtyfive/objectdetails/domain/usecase/implementation/GetPlaceUseCase.kt
@@ -1,6 +1,7 @@
 package ru.fivefourtyfive.objectdetails.domain.usecase.implementation
 
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.*
 import ru.fivefourtyfive.objectdetails.domain.datastate.PlaceDetailsDataState
 import ru.fivefourtyfive.objectdetails.domain.repository.abstraction.IPlaceDetailsRepository
@@ -16,26 +17,28 @@ class GetPlaceUseCase @Inject constructor(private val repository: IPlaceDetailsR
 
     override var dataBlocks: String? = null
 
-    override suspend fun execute(): Flow<PlaceDetailsViewState> = flow {
+    @Suppress("EXPERIMENTAL_IS_NOT_ENABLED")
+    @OptIn(FlowPreview::class)
+    override suspend fun execute(): Flow<PlaceDetailsViewState> =
         repository.getPlace(id, dataBlocks)
+            .flatMapConcat { flow { emit(reduce(it)) } }
             .catch { emit(PlaceDetailsViewState.Error()) }
-            .collect { emit(reduce((it))) }
-    }.flowOn(IO)
+            .flowOn(IO)
 
     override fun reduce(dataState: PlaceDetailsDataState): PlaceDetailsViewState {
         with(dataState) {
             return when (this) {
                 is PlaceDetailsDataState.Success -> PlaceDetailsViewState.Success(
-                    id          = place.id,
-                    title       = place.title,
+                    id = place.id,
+                    title = place.title,
                     description = place.description,
                     languageIso = place.languageIso,
-                    photos      = place.photos,
-                    comments    = place.comments,
-                    tags        = place.tags,
-                    lat         = place.lat,
-                    lon         = place.lon,
-                    location    = place.location
+                    photos = place.photos,
+                    comments = place.comments,
+                    tags = place.tags,
+                    lat = place.lat,
+                    lon = place.lon,
+                    location = place.location
                 )
                 is PlaceDetailsDataState.Loading -> PlaceDetailsViewState.Loading
                 is PlaceDetailsDataState.Error -> PlaceDetailsViewState.Error(message = message)
